bbeb54711bf1867ac84058745f867e5f31dc6e01
Wan Ji <wanji@live.com>
2014-06-22 17:10:58 +0800

    adding debug script

--- examples/cifar10.mpi/train_quick.sh
+++ examples/cifar10.mpi/train_quick.sh
@@ -2,8 +2,11 @@
 
 TOOLS=`pwd`/../../build/tools
 
-# MV4_DEBUG_CORESIZE=unlimited MV2_DEBUG_SHOW_BACKTRACE=1
-MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 mpirun -hostfile hosts -n 4 -wdir `pwd` $TOOLS/train_net.bin cifar10_quick_solver.prototxt
+MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 \
+mpirun -hostfile hosts -n 6 -wdir `pwd` \
+$TOOLS/train_net.bin cifar10_quick_solver.prototxt
 
 #reduce learning rate by fctor of 10 after 8 epochs
-MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 mpirun -hostfile hosts -n 4 -wdir `pwd` $TOOLS/train_net.bin cifar10_quick_solver_lr1.prototxt cifar10_quick_iter_1000.solverstate
+MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 \
+mpirun -hostfile hosts -n 6 -wdir `pwd` \
+$TOOLS/train_net.bin cifar10_quick_solver_lr1.prototxt cifar10_quick_iter_1000.solverstate
