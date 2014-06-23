f5aeb29228787950fb9c197a1c86ba87122a9108
Wan Ji <wanji@live.com>
2014-06-23 15:42:33 +0800

    calculate iterations w.r.t. number of MPI tasks automatically

--- examples/cifar10.mpi/train_quick_pdcc.sh
+++ examples/cifar10.mpi/train_quick_pdcc.sh
@@ -2,7 +2,7 @@
 
 TOOLS=`pwd`/../../build/tools
 
-NJOBS=4
+NJOBS=6
 N_TRAIN_JOBS=`expr $NJOBS - 2`
 N_FIRST_ITER=`expr 4000 / $N_TRAIN_JOBS`
 N_SECOND_ITER=`expr 5000 / $N_TRAIN_JOBS`
@@ -11,10 +11,10 @@ cat cifar10_quick_solver.prototxt.template | sed "s/FIRST_ITER/$N_FIRST_ITER/g" 
 cat cifar10_quick_solver_lr1.prototxt.template | sed "s/SECOND_ITER/$N_SECOND_ITER/g" > cifar10_quick_solver_lr1.prototxt
 
 MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 \
-mpirun -hostfile hosts -n 6 -wdir `pwd` \
+srun -n $NJOBS -p gpu --gres=gpu:3 --ntasks-per-node=3 \
 $TOOLS/train_net.bin cifar10_quick_solver.prototxt
 
 #reduce learning rate by fctor of 10 after 8 epochs
 MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 \
-mpirun -hostfile hosts -n 6 -wdir `pwd` \
+srun -n $NJOBS -p gpu --gres=gpu:3 --ntasks-per-node=3 \
 $TOOLS/train_net.bin cifar10_quick_solver_lr1.prototxt cifar10_quick_iter_$N_FIRST_ITER.solverstate
