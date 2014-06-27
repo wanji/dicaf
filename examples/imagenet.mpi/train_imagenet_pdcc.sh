b61eede69a2e18a670730759988926689050d0ea
Wan Ji <wanji@live.com>
2014-06-27 19:30:08 +0800

    update

--- examples/imagenet.mpi/train_imagenet_pdcc.sh
+++ examples/imagenet.mpi/train_imagenet_pdcc.sh
@@ -2,14 +2,14 @@
 
 TOOLS=`pwd`/../../build/tools
 
-NJOBS=7
+NJOBS=6
 N_TRAIN_JOBS=`expr $NJOBS - 2`
 N_MAX_ITER=`expr 450000 / $N_TRAIN_JOBS`
 
-cat imagenet_solver.prototxt.template | sed "s/MAX_ITER/$N_MAX_ITER/g" > imagenet_solver.prototxt
+cat imagenet_solver.prototxt.template | sed "s/MAX_ITER/$N_MAX_ITER/g" > imagenet_solver_pdcc.prototxt
 
 MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 \
-srun -n $NJOBS -p gpu --gres=gpu:3 --ntasks-per-node=4 \
-$TOOLS/train_net.bin imagenet_solver.prototxt
+srun -n $NJOBS -p gpu --gres=gpu:4 --ntasks-per-node=6 \
+$TOOLS/train_net.bin imagenet_solver_pdcc.prototxt
 
 echo "Done."
