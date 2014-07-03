43eebc4e5913f523b72b62181bd1b8ec7d8067a8
Wan Ji <wanji@live.com>
2014-07-03 11:17:55 +0800

    adjust info freq

--- examples/imagenet.mpi/train_imagenet_pdcc.sh
+++ examples/imagenet.mpi/train_imagenet_pdcc.sh
@@ -3,13 +3,15 @@
 TOOLS=`pwd`/../../build/tools
 
 NJOBS=6
+N_TASKS_PER_NODE=6
+GRES_GPU=4
 N_TRAIN_JOBS=`expr $NJOBS - 2`
 N_MAX_ITER=`expr 450000 / $N_TRAIN_JOBS`
 
 cat imagenet_solver.prototxt.template | sed "s/MAX_ITER/$N_MAX_ITER/g" > imagenet_solver_pdcc.prototxt
 
 MV2_ENABLE_AFFINITY=0 MV2_USE_CUDA=1 GLOG_logtostderr=1 \
-srun -n $NJOBS -p gpu --gres=gpu:4 --ntasks-per-node=6 \
+srun -n $NJOBS -p gpu --gres=gpu:$GRES_GPU --ntasks-per-node=$N_TASKS_PER_NODE \
 $TOOLS/train_net.bin imagenet_solver_pdcc.prototxt
 
 echo "Done."
